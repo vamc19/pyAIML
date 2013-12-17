fb793ea936a1b8dce12cf3ad8ddd526a058167dc
Vamsi Krishna Atluri <vamc19@gmail.com>
2013-12-17 14:58:55 +0530

    Added function tag.

---
+++ test_function.py
@@ -0,0 +1,14 @@
+__author__ = 'yos'
+
+
+def hello_world():
+    return "Executed function `hello_world`."
+
+
+def add(a, b):
+    return str(int(a)+int(b))
+
+
+def test_bed(s):
+    print s
+    return s
